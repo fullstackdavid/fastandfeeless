8e5493c4d5f5ac133c142abc9209980fcf2c2e2e
david <fullstackdahveed@gmail.com>
2019-12-28 15:14:17 +0530

    add global volume and price change percent.

--- App.js
+++ App.js
@@ -18,21 +18,34 @@ async function getNANOfromCMC() {
     return JSON.stringify(data["data"].NANO, null, "\t");
 }
 
-var price, mcap , cmcrank, hrchange;
+var price, mcap , cmcrank, hrchange, volume_24h, price_trend;
 
 getNANOfromCMC().then(function(res){
             var result = JSON.parse(res);
             cmcrank = result.cmc_rank;
             mcap = result.quote.USD.market_cap;
             price = result.quote.USD.price;
+            volume_24h = result.quote.USD.volume_24h;
             hrchange = result.quote.USD.percent_change_1h;
             hrchange = Math.round(hrchange * 100) / 100;
             mcap= mcap / 1000000;
             mcap= Math.round(mcap * 100) / 100;
             price= Math.round(price * 1000) / 1000;
-            finalTweet = 'Price : $'+price+
-            '\r\n\n\tMarket cap : $'+mcap +'M' +'\r\n\n\tCoinmarketcap rank : '+cmcrank 
-            +'\r\n\n\tFees per tx : $0.00'
+            volume_24h= volume_24h / 1000000;
+            volume_24h= Math.round(volume_24h * 1000) / 1000;
+            console.log(result.quote.USD.percent_change_7d);
+            console.log(volume_24h);
+            console.log(hrchange);
+            if(hrchange>=0){
+                price_trend = '📈';
+            } else {
+                price_trend = '📉';
+            }
+            finalTweet = 'Price : $'+price
+            +'\r\n\n\tPrice change (last 1h) : '+hrchange+'%' +' '+ price_trend
+            +'\r\n\n\tMarket cap : $'+mcap +'M' 
+            +'\r\n\n\tCoinmarketcap rank : '+cmcrank 
+            +'\r\n\n\tVolume (last 24h) : $'+volume_24h +'M' 
             +'\r\n\n\t$NANO';
             if(hrchange >10){
                 if(hrchange > 100 ){
@@ -48,13 +61,13 @@ getNANOfromCMC().then(function(res){
             } 
 
             console.log(finalTweet);
-            twitterClient.post('statuses/update', {status: finalTweet})
-            .then(function (tweet) {
-                 console.log(tweet);
-                })
-                .catch(function (error) {
-                    console.log(error)
-                    throw error;
-                });
+            // twitterClient.post('statuses/update', {status: finalTweet})
+            // .then(function (tweet) {
+            //      console.log(tweet);
+            //     })
+            //     .catch(function (error) {
+            //         console.log(error)
+            //         throw error;
+            //     });
 
         })
